54b96bb7074be5c551b93ac4fbbe9799f09e58e4
Jesus Noland <jn@jesusnoland.com>
2016-12-04 00:31:49 -0800

    Basic log in with no session

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -9,13 +9,10 @@ var partitionKey = nconf.get("PARTITION_KEY");
 var accountName = nconf.get("STORAGE_NAME");
 var accountKey = nconf.get("STORAGE_KEY");
 
-var passport = require('passport');
-var passportLocalStrategy = require('passport-local').Strategy;
-
 
 var express = require('express'),
-  router = express.Router(),
-  Article = require('../models/article');
+    router = express.Router(),
+    expressSession = require('express-session');
 
 var UserList = require('./userlist');
 var User = require('../models/user');
@@ -24,43 +21,17 @@ var userList = new UserList(user);
 
 // Session
 var app = express();
-var session = require('express-session');
-app.use(session({
+app.use(expressSession({
     secret: '777F',
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        secure: true,
+        maxAge: 30000
+    }
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, true);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, false);
-  });
-});
-
-passport.use(new passportLocalStrategy(
-  function(username, password, done) {
-    userList.findOne({ username: username, password: password }, function(err, user) {
-      console.log("user created");
-      return done(null, true, { message: true });
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
 
 module.exports = function (app) {
   app.use('/', router);
@@ -80,13 +51,20 @@ router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
-router.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login'})
-);
+router.post('/login', function (req, res) {
+  console.log(req.body);
+  if (!req.body.username || !req.body.password) {
+    res.redirect('/');
+  } else {
+    if (req.body.username === "wap" && req.body.password === "test") {
+      res.redirect('/dashboard');
+    }
+  }
+  res.redirect('/login');
+});
 
 router.get('/signin', function (req, res, next) {
-	res.render('SignUp');
+  res.render('SignUp');
 });
 
 router.get('/dashboard', function (req, res, next) {
